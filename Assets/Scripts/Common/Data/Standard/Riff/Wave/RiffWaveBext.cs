013f61d19226d85fe5d1c4f3472ea6bb7da32900
monoamp <monoamp@users.noreply.github.com>
2015-01-24 09:51:41 +0900

    ソースコードの文字コード問題対処.

--- Assets/Scripts/Common/Data/Standard/Riff/Wave/RiffWaveBext.cs
+++ Assets/Scripts/Common/Data/Standard/Riff/Wave/RiffWaveBext.cs
@@ -8,18 +8,18 @@ namespace Monoamp.Common.Data.Standard.Riff.Wave
 	{
 		public const string ID = "bext";
 
-		public readonly string description;			// ASCII or S-JIS, Description of the sound sequence
-		public readonly string originator;			// ASCII or S-JIS, 制作会社、制作者名
-		public readonly string originatorReference;	// ASCII or S-JIS, 制作ユニークコード
-		public readonly string originationDate;		// ASCII, 制作年月日 yyyy-mm-dd
-		public readonly string originationTime;		// ASCII, 制作時刻 hh:mm:ss
-		public readonly UInt32 timeReferenceLow;	// First sample count since midnight, low word
-		public readonly UInt32 timeReferenceHigh;	// First sample count since midnight, high word
-		public readonly UInt16 version;				// BWF-Jのバージョン番号
-		public readonly Byte[] umid;				// Binary Byte 0 of SMPTE UMID
-		//public readonly Byte umid_63;				// Binary Byte 63 of SMPTE UMID
-		public readonly Byte[] reserved;			// 190 Bytes, reserved for future use, set to “NULL”
-		public readonly string codingHistory;		// ASCII or S-JIS, Coding history
+		public readonly string description;	
+		public readonly string originator;
+		public readonly string originatorReference;
+		public readonly string originationDate;
+		public readonly string originationTime;
+		public readonly UInt32 timeReferenceLow;
+		public readonly UInt32 timeReferenceHigh;
+		public readonly UInt16 version;
+		public readonly Byte[] umid;
+		//public readonly Byte umid_63;
+		public readonly Byte[] reserved;
+		public readonly string codingHistory;
 
 		public RiffWaveBext( string aId, UInt32 aSize, ByteArray aByteArray, RiffChunkList aParent )
 			: base( aId, aSize, aByteArray, aParent )
